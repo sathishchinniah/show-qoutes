1b34a0e1e630ec396a3750367508a611f4f44191
sathish chinniah <sathish73ec@gmail.com>
2015-09-27 23:33:03 +0530

    init

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Sathish Chinniah on 23/09/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFact() {
+        let randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+
+}
+
